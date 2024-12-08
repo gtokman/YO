bc059b8df8756f4af9b28649f7d0ab0ddb8dd224
Gary Tokman <yo@6ary.com>
2024-12-08 10:14:45 -0500

    feat: create ios and backend api

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Gary Tokman on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
